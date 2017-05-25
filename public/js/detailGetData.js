c0cb02da8da14ff9270a97099310797940610ede
Jeson <jesonhu_web@163.com>
2017-05-25 18:21:57 +0800

    5.25 详情页布局一半

--- public/js/detailGetData.js
+++ public/js/detailGetData.js
@@ -1,21 +1,41 @@
 new Vue({
     el: ".body-hook",
     data: {
-        goods: null
+        goods: null, // 商品详情
+        sameList: [], // 相似产品
+        hotSale: [], // 热销
+        topicsRecommended: [] // 专题推荐
     },
     mounted() {
         this.$nextTick(() => {
             this.conView();
         });
     },
-    filtes: {
-
+    filters: {
+        rmb(val, str) {
+            if (typeof str === 'undefined') {
+                return `${val}.00`;
+            } else {
+                return `${str}${val}.00`;
+            }
+        },
+        wan(val) {
+            const nVal = Number(val);
+            if (nVal >=1000) {
+                return `999+`;
+            } else {
+                return val;
+            }
+        }
     },
     methods: {
         conView() {
             axios.get('/api/detail.json').then((res) => {
                 if (res.status === 200) {
                     this.goods = res.data.result.goodsDetail;
+                    this.sameList = res.data.result.sameList;
+                    this.hotSale = res.data.result.hotSale;
+                    this.topicsRecommended = res.data.result.topicsRecommended;
                 }
             })
         }
