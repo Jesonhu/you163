d33322b1bb82f449f75442694500274872d7b2e6
jesonhu <jesonhu_web@163.com>
2017-05-31 00:19:13 +0800

    5.30 nuxt使用

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,16 +7,25 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'keyworlds', name: 'keyworlds', content: '全站关键词' },
+      { hid: 'description', name: 'description', content: '全站关键词描述' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '/assets/css/reset.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/reset.css' }
     ]
   },
+  /**
+   * 全局css配置
+   */
+  css: [
+    '~assets/css/reset.css' // 全局添加内部样式
+  ],
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: 'red' }, // 刷新条颜色
   /*
   ** Build configuration
   */
@@ -25,14 +34,14 @@ module.exports = {
     ** Run ESLINT on save
     */
     extend (config, ctx) {
-      if (ctx.isClient) {
+      /*if (ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
-      }
+      }*/
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,42 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
-      <h1 class="title">
-        NUXT
-      </h1>
-      <h2 class="subtitle">
-        Universal Vue.js Application
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+      首页内容
     </div>
+    <p><nuxt-link to="/about">关于我们</nuxt-link></p>
   </section>
 </template>
 
 <script>
-import Logo from '~components/Logo.vue'
 
 export default {
+  data() {
+    return {
+      title: '首页sss'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {hid: 'keyworlds', name: 'keyworlds', content: '首页关键词'},
+        {hid: 'description', name: 'description', content: '首页关键词描述'}
+      ]
+    }
+  },
   components: {
-    Logo
   }
 }
 </script>
 
 <style>
-.container
-{
+.container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+  background-color:blue;
 }
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
+
 </style>
