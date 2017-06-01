3d32647cbf2ee152a09ef15de1ad5a6ff5d89269
jesonhu <jesonhu_web@163.com>
2017-06-01 23:47:34 +0800

    6.1 登录页面布局完善

--- pages/login.vue
+++ pages/login.vue
@@ -8,36 +8,63 @@
 
     <!-- 登录 -->
     <div class="pd-login">
-      <div class="container">
-        <div class="pd-login-main">
-          <div class="pd-loginformwrap">
-            <div class="pd-login-title">网易帐号登录</div>
-            <div class="pd-login-body">
-              <form id="login-form">
-                <div class="usernamewrap">
-                  <div class="user-icon"></div>
-                  <div class="user-input">
-                    <input type="text" class="username">
-                    <div class="auto-remember"></div>
-                  </div>
-                  <i class="pd-login-remove-icon"></i>
+        <div class="container">
+            <div class="pd-login-main">
+                <div class="pd-loginformwrap">
+                    <div class="pd-login-title">网易帐号登录</div>
+                    <div class="pd-login-body">
+                        <form id="login-form">
+                            <div class="usernamewrap">
+                                <div class="user-icon"></div>
+                                <div class="user-input">
+                                    <input type="text" class="username">
+                                    <div class="auto-remember"></div>
+                                </div>
+                                <i class="pd-login-remove-icon"></i>
+                            </div>
+                            <div class="pwdwrap">
+                                <div class="pwd-icon"></div>
+                                <div class="pwd-input">
+                                    <input type="password" class="passworld">
+                                </div>
+                                <i class="pd-login-remove-icon"></i>
+                            </div>
+                            <div class="c-loginerror">
+                                <i class="error-icon"></i>
+                                <div class="error-text">请输入账号</div>
+                            </div>
+                            <div class="submitwrap">
+                                <button class="submit">登录</button>
+                            </div>
+                            <div class="unloginwrap">
+                                <a href="" class="forgetpwd">忘记密码</a>
+                                <a href="" class="reg">去注册</a>
+                            </div>
+                            <div class="ohtermethod">
+                                <div class="title">
+                                    <span class="text z10">其他登录方式</span>
+                                    <hr class="line z5">
+                                </div>
+                                <div class="bd">
+                                    <a href="" class="method wx">
+                                        <i class="method-icon wx-icon"></i>
+                                        <span class="text">微信</span>
+                                    </a>
+                                    <a href="" class="method qq">
+                                        <i class="method-icon qq-icon"></i>
+                                        <span class="text">QQ</span>
+                                    </a>
+                                    <a href="" class="method wb">
+                                        <i class="method-icon wb-icon"></i>
+                                        <span class="text">微博</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div class="pwdwrap">
-                  <div class="pwd-icon"></div>
-                  <div class="pwd-input">
-                    <input type="password" class="passworld">
-                  </div>
-                  <i class="pd-login-remove-icon"></i>
-                </div>
-                <div class="c-loginerror">
-                  <i class="error-icon"></i>
-                  <div class="error-text">请输入账号</div>
-                </div>
-              </form>
             </div>
-          </div>
         </div>
-      </div>
     </div>
 
     <!-- 右侧固定导航 -->
